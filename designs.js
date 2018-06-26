c78d64a16c336d2ad75a90493c53ce6f98a7bf95
Ayoola Olugbenga Daniel <ayoolaolugbengadaniel4@gmail.com>
2018-06-26 17:21:19 +0100

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,34 @@
+// Select color input
+// Select size input
+var height, width, color;
+// When size is submitted by the user, call makeGrid()
+$("#sizePicker").submit(function () {
+    event.preventDefault();
+    height = $("#inputHeight").val();
+    width = $("#inputWidth").val();
+    makeGrid(height, width);
+
+});
+// Creating table rows and table data
+function makeGrid(height, width) {
+    $("tr").remove();
+for (var i = 1; i <= height; i++) {
+       $("#pixelCanvas").append("<tr></tr>");
+           for (var j = 0; j < width; j++) {
+             $('tr:last-child').append('<td></td>');
+             $('td').attr("class", 'color');
+           }
+}
+
+// Adding color to table data
+// When td is clicked by the user, change color of td
+
+$('.color').click(function(event){
+        var paint = $('#colorPicker').val();
+        $(event.target).css('background-color', paint);
+    });
+
+}
+
+
+
